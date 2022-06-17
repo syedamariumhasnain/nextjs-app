14d52a0d5c45932447270b4522c8a13d968be3a7
syedamariumhasnain <syedamarium27@gmail.com>
2022-06-17 12:59:49 +0500

    Added Layout, Link

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import Layout from "../components/layout/Layout";
+import "../styles/globals.css";
+
+// some commponents or settings that affects all your pages
+// are done inside _app.js file i.e. Layout 
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
